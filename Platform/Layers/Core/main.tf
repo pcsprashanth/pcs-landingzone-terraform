98146ab5b33f6eaf214cadd096345f8c1eac36db
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-05-17 14:32:51 +0530

    now

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -18,7 +18,7 @@ data "azurerm_client_config" "core" {}
 
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "4.0.1"
+  version = "3.54.0"
 
   providers = {
     azurerm              = azurerm
