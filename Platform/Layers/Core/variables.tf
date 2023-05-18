88d5057efd75013213d595cc167e8a8ca9f2297e
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-05-18 20:30:31 +0530

    done

--- Platform/Layers/Core/variables.tf
+++ Platform/Layers/Core/variables.tf
@@ -1,9 +1,9 @@
 variable "root_id" {
   type    = string
-  default = "mg-itaudev"
+  default = "mg-dev"
 }
 
 variable "root_name" {
   type    = string
-  default = "Itaudev"
+  default = "pcsdev"
 }
