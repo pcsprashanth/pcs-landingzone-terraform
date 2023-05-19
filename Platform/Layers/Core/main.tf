5c4b091f1c8822ef5c062410671adc36da222643
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-05-19 13:04:22 +0530

    pushnow12

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -163,27 +163,22 @@ module "enterprise_scale" {
       }
     }
 }
-
-module "policyset" {
-  source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "3.0.0"
-
- providers = {
-    azurerm              = azurerm
-    azurerm.connectivity = azurerm
-    azurerm.management   = azurerm         
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
   }
+}
+provider "azurerm" {
+  features {}
+}
 
-  root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = var.root_id
-  root_name      = var.root_name
-
-
-resource "azurerm_management_group_policy_assignment" "asb" {
-          name                 = "Azure-Security-BenchMark"
-          management_group_id  = "mg-itaudev"
-          policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
-          description          = "The Azure Security Benchmark initiative represents the policies and controls implementing security recommendations defined in Azure Security Benchmark v3, see https://aka.ms/azsecbm. This also serves as the Microsoft Defender for Cloud default policy initiative. You can directly assign this initiative, or manage its policies and compliance results within Microsoft Defender for Cloud."
-          display_name         = "Security Benchmark"
-  }
+resource "azurerm_subscription_policy_assignment" "asb" {
+  name                 = "Cloud-BenchMark"
+  subscription_id      = var.cust_scope
+  policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
+  description          = "The Azure Security Benchmark initiative represents the policies and controls implementing security recommendations defined in Azure Security Benchmark v3, see https://aka.ms/azsecbm. This also serves as the Microsoft Defender for Cloud default policy initiative. You can directly assign this initiative, or manage its policies and compliance results within Microsoft Defender for Cloud."
+  display_name         = "Security Benchmark"
 }
