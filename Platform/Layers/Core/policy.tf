461e6ac519d3420eb45a5f555bfc095e351a5409
pcsprashanth <pcsprashanth@users.noreply.github.com>
2023-05-19 13:52:59 +0530

    pushnow

--- Platform/Layers/Core/policy.tf
+++ Platform/Layers/Core/policy.tf
@@ -1,10 +1,8 @@
-resource "azurerm_management_group" "mg-itau1" {
-  display_name = "mg-itau"
-}
+data "azurerm_subscription" "current" {}
 
 resource "azurerm_management_group_policy_assignment" "asb" {
   name                 = "Cloud-BenchMark"
-  management_group_id  = azurerm_management_group.mg-itau1.id
+  subscription_id  = data.azurerm_subscription.current.id
   policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
   description          = "The Azure Security Benchmark initiative represents the policies and controls implementing security recommendations defined in Azure Security Benchmark v3, see https://aka.ms/azsecbm. This also serves as the Microsoft Defender for Cloud default policy initiative. You can directly assign this initiative, or manage its policies and compliance results within Microsoft Defender for Cloud."
   display_name         = "Security Benchmark"
