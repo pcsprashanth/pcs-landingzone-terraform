7dd2e475773d05b0cd31039c2ca0596bcbc25224
Chandrasekhara <v-cpathaneni@microsoft.com>
2023-06-22 20:31:12 +0530

    checknow12

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -86,15 +86,9 @@ module "enterprise_scale" {
           archetype_id   = "es_sandboxes"
           parameters     = {
             ITAU-defender-default = {
-              bringYourOwnUserAssignedManagedIdentity= {
-                 false
-              }
-             userAssignedManagedIdentityName = [
-                "itau-managed-identity"
-             ],
-             userAssignedManagedIdentityResourceGroup = [
-                "ITAUTEST"
-             ]
+             bringYourOwnUserAssignedManagedIdentity= false,
+             userAssignedManagedIdentityName = "itau-managed-identity",
+             userAssignedManagedIdentityResourceGroup = "ITAUTEST"
           }
         }
           access_control = {}
