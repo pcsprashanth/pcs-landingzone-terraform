e7d1d13ce8c0ffb93891797a68897fab22cbd067
Chandrasekhara <v-cpathaneni@microsoft.com>
2023-06-27 21:36:38 +0530

    pushnow12

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -84,13 +84,7 @@ module "enterprise_scale" {
         subscription_ids           = []
         archetype_config = {
           archetype_id   = "es_sandboxes"
-          parameters     = {
-            ITAU-defender-udefined = {
-              bringYourOwnUserAssignedManagedIdentity = true,
-              userWorkspaceResourceId = "abc"
-              workspaceRegion = "Brazil"
-              }
-          }
+          parameters     = {}
           access_control = {}
         }
         # depends_on= [azurerm_subscription.management1]
