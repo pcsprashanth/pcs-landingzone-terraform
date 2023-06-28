00f6ae9d7f856b0baa1a01fc4dce6a27870c432b
Chandrasekhara <v-cpathaneni@microsoft.com>
2023-06-28 20:31:55 +0530

    checknow

--- Platform/Layers/Core/main.tf
+++ Platform/Layers/Core/main.tf
@@ -84,13 +84,7 @@ module "enterprise_scale" {
         subscription_ids           = []
         archetype_config = {
           archetype_id   = "es_sandboxes"
-          parameters     = {
-            itau_virtual_machine_ss = {
-              bringYourOwnUserAssignedManagedIdentity = false,
-              userAssignedIdentityName = var.mi_id,
-              identityResourceGroup = var.mi_rg_id
-            }
-          }
+          parameters     = {}
           access_control = {
             Reader = var.reader_access_id
           }
